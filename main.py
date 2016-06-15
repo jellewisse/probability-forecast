Bug fix: pass standard deviation to scipy Norm instead of variance.

main.py
@@ -14,10 +14,139 @@ from helpers import data_io
 from helpers import metrics
 import helpers.plotting as plot
 from helpers import data_assimilation
+from helpers import verification
 
 # Configure cache
 CACHE_DIR = '.cache'
 cache = pyfscache.FSCache(CACHE_DIR)
+
+
+def main(element_name, model_names, station_names, train_days, issue,
+         forecast_hours, forecast_hour_group_size):
+    """Main ETL method."""
+    logging.info("Loading data..")
+    load_start_time = time()
+    full_data = cached_load_data(
+        element_name,
+        station_names,
+        issue,
+        model_names
+    )
+    logging.info("Done loading data (%ds).", time() - load_start_time)
+    _log_data_shape(full_data)
+
+    # Drop unrequested forecast hours
+    data_assimilation.filter_unused_forecast_hours(full_data, forecast_hours)
+
+    # TODO TdR 01.06.16 : Temporarily write dataset to file for debugging.
+    # data_io.write_csv_for_r_package(full_data, 'dataset_for_r.csv')
+
+    # Ensemble definition
+    ensemble_grouping, ensemble_columns = \
+        _model_to_group(model_names, element_name)
+    observation_column = element_name + '_OBS'
+    model_mix = mixture_model.GaussianMixtureModel(ensemble_grouping)
+
+    # Bias corrector definition
+    model_bias = bias_corrector.SimpleBiasCorrector(
+        len(ensemble_columns), ensemble_grouping)
+
+    # Loop over forecast hour groups
+    forecast_hour_grouping = full_data.groupby(
+        _get_group_index(full_data.forecast_hour, forecast_hour_group_size)
+    )
+    for count, (group_id, data) in enumerate(forecast_hour_grouping):
+        fh_time = time()
+        logging.info("Processing %d / %d forecast hour groups..",
+                     count + 1, len(forecast_hour_grouping))
+        if len(data) == 0:
+            logging.warn("No data for forecast hour group %d. Skipping.",
+                         count)
+            continue
+
+        # The dates to predict for.
+        dates_in_group = data['valid_date'].unique()
+        hours_in_group = data.forecast_hour.unique()
+        assert len(dates_in_group) * len(station_names) == len(data), \
+            "Each station valid date should only have a single prediction"
+
+        # Variables for plotting
+        model_mix_weights = []
+        model_mix_variances = []
+        model_bias_intercepts = []
+        plot_valid_dates = []
+
+        # Moving window prediction
+        # TODO TdR 31.05.16 : Loop over prediction dates instead of rows
+        for index, row in data.iterrows():
+            logging.debug("Starting with row %s", str(row['valid_date']))
+            # If one of the model prediction forecasts is unavailable, skip.
+            if row[ensemble_columns + [observation_column]].isnull().any():
+                continue
+
+            # Select train data
+            valid_date = row['valid_date']
+            latest_forecast_hour = hours_in_group[-1]
+            # TODO TdR 31.05.16 : select test examples as well.
+            train_data = data_assimilation.select_train_data(
+                data, latest_forecast_hour, valid_date, train_days,
+                ensemble_columns, observation_column
+            )
+
+            # Check data quality
+            train_dates = train_data['valid_date'].unique()
+            if not data_assimilation.check_data_coverage(
+               train_dates, train_days):
+                # logging.info(
+                #     "Not enough training days (%s / %d), skipping date %s",
+                #     str(len(train_data)).zfill(2), train_days,
+                #     str(valid_date))
+                continue
+
+            X_train = train_data[ensemble_columns].as_matrix()
+            y_train = train_data[observation_column].as_matrix()
+            X_test = row[ensemble_columns].as_matrix()
+            y_test = row[observation_column]
+
+            # Train bias model
+            logging.debug("Training bias model..")
+            model_bias.fit(X_train, y_train)
+            X_train = model_bias.predict(X_train)
+
+            # Train mixture model
+            logging.debug("Training mixture model..")
+            model_mix.fit(X_train, y_train)
+
+            model_mix_weights.append(model_mix.weights)
+            model_mix_variances.append(model_mix.get_member_variances())
+            model_bias_intercepts.append(model_bias.intercept_per_model)
+            plot_valid_dates.append(valid_date)
+
+            # Predict
+            logging.debug("Storing predictions..")
+            X_test = model_bias.predict(X_test)
+            model_mix.set_member_means(X_test)
+
+            # Add predictions to dataset
+            PERCENTILES = np.array([5, 10, 25, 50, 75, 90, 95])
+            add_model_predictions(
+                full_data, model_mix, y_test, index, element_name, PERCENTILES)
+            add_model_parameters(
+                full_data, index, model_mix, model_bias, ensemble_columns)
+        logging.info("Done with forecast hour group %d (%.2fs).",
+                     group_id, time() - fh_time)
+
+        # Plotting per forecast hour
+        for forecast_hour in hours_in_group:
+            for station_name in station_names:
+                plot.plot_ensemble_percentiles(
+                    forecast_hour, PERCENTILES, element_name, station_name,
+                    full_data)
+        plot.plot_model_parameters(
+            plot_valid_dates, model_mix_weights, model_mix_variances,
+            model_bias_intercepts,
+            hours_in_group, ensemble_columns, element_name)
+    return full_data.drop(ensemble_columns, axis=1)
 
 
 @cache
@@ -93,139 +222,18 @@ def add_model_predictions(dataframe, model, test_observation, row_index,
         dataframe.loc[row_index, name] = value
 
 
-def main(element_name, model_names, station_names, train_days, issue,
-         forecast_hours, forecast_hour_group_size):
-    """Main ETL method."""
-    logging.info("Loading data..")
-    load_start_time = time()
-    full_data = cached_load_data(
-        element_name,
-        station_names,
-        issue,
-        model_names
-    )
-    logging.info("Done loading data (%ds).", time() - load_start_time)
-    _log_data_shape(full_data)
+def add_model_parameters(dataframe, row_index, mixture_model,
+                         bias_model, model_identifiers):
 
-    # Drop unrequested forecast hours
-    data_assimilation.filter_unused_forecast_hours(full_data, forecast_hours)
-
-    # Ensemble definition
-    ensemble_grouping, ensemble_columns = \
-        _model_to_group(model_names, element_name)
-    observation_column = element_name + '_OBS'
-    model_mix = mixture_model.GaussianMixtureModel(
-        len(ensemble_columns), ensemble_grouping)
-
-    # Bias corrector definition
-    model_bias = bias_corrector.SimpleBiasCorrector(
-        len(ensemble_columns), ensemble_grouping)
-
-    # Loop over forecast hour groups
-    forecast_hour_grouping = full_data.groupby(
-        _get_group_index(full_data.forecast_hour, forecast_hour_group_size)
-    )
-    for group_id, data in forecast_hour_grouping:
-        fh_time = time()
-        logging.info("Processing %d / %d forecast hour groups..",
-                     group_id, len(forecast_hour_grouping))
-        if len(data) == 0:
-            logging.warn("No data for forecast hour group %d. Skipping.",
-                         group_id)
-            continue
-
-        # The dates to predict for.
-        dates_in_group = data['valid_date'].unique()
-        hours_in_group = data.forecast_hour.unique()
-        assert len(dates_in_group) * len(station_names) == len(data), \
-            "Each station valid date should only have a single prediction"
-
-        # Variables for plotting
-        model_mix_weights = []
-        model_mix_variances = []
-        model_bias_intercepts = []
-        plot_valid_dates = []
-
-        # Moving window prediction
-        # TODO TdR 31.05.16 : Loop over prediction dates instead of rows
-        for index, row in data.iterrows():
-            logging.debug("Starting with row %s", str(row['valid_date']))
-            # If one of the model prediction forecasts is unavailable, skip.
-            if row[ensemble_columns + [observation_column]].isnull().any():
-                continue
-
-            # Select train data
-            valid_date = row['valid_date']
-            latest_forecast_hour = hours_in_group[-1]
-            # TODO TdR 31.05.16 : select test examples as well.
-            train_data = data_assimilation.select_train_data(
-                data, latest_forecast_hour, valid_date, train_days,
-                ensemble_columns, observation_column
-            )
-
-            # Check data quality
-            train_dates = train_data['valid_date'].unique()
-            if not data_assimilation.check_data_coverage(
-              train_dates, train_days):
-                logging.warn(
-                    "Not enough training days (%s / %d), skipping date %s",
-                    str(len(train_data)).zfill(2), train_days,
-                    str(valid_date))
-                continue
-
-            X_train = train_data[ensemble_columns].as_matrix()
-            y_train = train_data[observation_column].as_matrix()
-            X_test = row[ensemble_columns].as_matrix()
-            y_test = row[observation_column]
-
-            # Train bias model
-            logging.debug("Training bias model..")
-            model_bias.fit(X_train, y_train)
-            X_train = model_bias.predict(X_train)
-
-            # Train mixture model
-            logging.debug("Training mixture model..")
-            model_mix.fit(X_train, y_train)
-            model_mix_weights.append(model_mix.weights)
-            model_mix_variances.append(model_mix.get_member_variances())
-            model_bias_intercepts.append(model_bias.intercept_per_model)
-            plot_valid_dates.append(valid_date)
-
-            # Predict
-            logging.debug("Storing predictions..")
-            X_test = model_bias.predict(X_test)
-            model_mix.set_member_means(X_test)
-
-            # Add predictions to dataset
-            PERCENTILES = np.array([5, 10, 25, 75, 90, 95])
-            add_model_predictions(
-                full_data, model_mix, y_test, index, element_name, PERCENTILES)
-        logging.info("Done with forecast hour group %d (%.2fs).",
-                     group_id, time() - fh_time)
-        for forecast_hour in hours_in_group:
-            for station_name in station_names:
-                plot.plot_ensemble_percentiles(
-                    forecast_hour, PERCENTILES, element_name, station_name,
-                    full_data)
-        plot.plot_model_parameters(
-            plot_valid_dates, model_mix_weights, model_mix_variances,
-            model_bias_intercepts,
-            hours_in_group, ensemble_columns, element_name)
-    return full_data.drop(ensemble_columns, axis=1)
-
-
-def do_verification(data, forecast_hour):
-    """Method for running several plotting routines."""
-    hourly_data = data[data.forecast_hour == forecast_hour]
-    plot.plot_verification_rank_histogram(hourly_data)
-    plot.plot_PIT_histogram(hourly_data)
-    # a, b, c = plot.calculate_threshold_hits(hourly_data)
-    # plot.plot_relialibilty_sharpness_diagram(a, b, c)
-    mean_crps = hourly_data['2T_CRPS'].mean()
-    logging.info("Mean CRPS: %f", mean_crps)
-    deterministic_MAE = \
-        abs(hourly_data['2T_ENSEMBLE_MEAN'] - hourly_data['2T_OBS']).mean()
-    logging.info("Ensemble mean MAE: %f", deterministic_MAE)
+    model_weights = mixture_model.weights
+    model_variances = mixture_model.get_member_variances()
+    model_biases = bias_model.intercept_per_model
+    model_iterator = \
+        zip(model_identifiers, model_weights, model_variances, model_biases)
+    for model_id, weight, variance, bias in model_iterator:
+        dataframe.loc[row_index, model_id + '_weight'] = weight
+        dataframe.loc[row_index, model_id + '_variance'] = variance
+        dataframe.loc[row_index, model_id + '_bias'] = bias
 
 
 def load_configuration(configuration_name='main'):
@@ -243,15 +251,22 @@ def log_dict(dictionary):
     logging.info("End of configuration parameters.")
 
 
-def write_predictions(data_frame, element_name, model_names, forecast_hours):
+def write_predictions(dataframe, element_name, model_names, forecast_hours):
     """Write a data_frame to file."""
     # Write predictions to file
-    data_frame.sort_values(['issue_date', 'forecast_hour'],
-                           ascending=True, inplace=True)
-    file_path = 'output/' + element_name + '_' + '_'.join(model_names) + '_' \
+    dataframe.sort_values(['issue_date', 'forecast_hour'],
+                          ascending=True, inplace=True)
+    file_path = 'output/predictions_' + element_name + '_' + '_'.join(model_names) + '_' \
         + str(forecast_hours[0]) + '_' + str(forecast_hours[-1]) + '.csv'
-    data_io.write_csv(data_frame, file_path)
+    data_io.write_csv(dataframe, file_path)
 
+
+def write_verification(dataframe, element_name, model_names, forecast_hours):
+    file_path = 'output/verification_' + element_name + '_' + '_'.join(model_names) + '_' \
+        + str(forecast_hours[0]) + '_' + str(forecast_hours[-1]) + '.csv'
+    # Use pandas to_csv instead of the helper write_csv, since we use forecast
+    # hour as index for verification results.
+    dataframe.to_csv(file_path)
 
 if __name__ == "__main__":
     """Run the probability forecast.
@@ -260,20 +275,20 @@ if __name__ == "__main__":
         python main.py [configuration_name]
     """
 
-    # Logger setup
-    logging.basicConfig(
-        format='%(asctime)s - %(levelname)s - %(message)s',
-        level='DEBUG'
-    )
-    logging.info("Starting program.")
-
     if len(sys.argv) >= 2:
         config_section_name = sys.argv[1]
     else:
         config_section_name = 'main'
 
     config = load_configuration(config_section_name)
-    logging.info("Configuration loaded.")
+    logging_level = config['logging_level']
+    # Logger setup
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        level=logging_level
+    )
+    logging.info("Starting program.")
+
     log_dict(config)
 
     # Forecast hour configuration
@@ -297,6 +312,11 @@ if __name__ == "__main__":
         element_name, model_names, station_names, train_days,
         model_issue, forecast_hours, fh_group_size)
 
-    # Write predicitons to file
+    logging.info("Running verification..")
+    verification_results = \
+        verification.run_verification_per_hour(data, config)
+    # Write results to file
     write_predictions(data, element_name, model_names, forecast_hours)
+    write_verification(
+        verification_results, element_name, model_names, forecast_hours)
     logging.info("Finished program.")

mixture_model/base.py
@@ -6,7 +6,7 @@ import numpy as np
 class MixtureModel(object, metaclass=abc.ABCMeta):
     """Base class for statistical model mixtures."""
 
-    def __init__(self, member_count, distribution):
+    def __init__(self, member_count, distribution, member_names=None):
         """Initialize a mixture model with a specific distribution.
 
         Parameters
@@ -16,8 +16,10 @@ class MixtureModel(object, metaclass=abc.ABCMeta):
         member_count : integer
             Number of members to initialize the mixture with
         """
+        if member_names is None:
+            member_names = [''] * member_count
         self._members = \
-            [MixtureMember(distribution) for _ in range(member_count)]
+            [MixtureMember(distribution, name) for name in member_names]
         self.member_count = member_count
         self.weights = np.ones(member_count) / member_count
 
@@ -67,16 +69,16 @@ class MixtureModel(object, metaclass=abc.ABCMeta):
 class MixtureMember(object):
     """Base class for mixture member."""
 
-    def __init__(self, distribution):
+    def __init__(self, distribution, name=''):
         """Initialize the member with a distribution class."""
         self.distribution = distribution  # Distribution function
+        self.name = name
         self.parameters = {}  # Distribution function keyword arguments
-        self.bias = 0  # Additive bias vector
 
     def pdf(self, X):
         """Compute the member PDF."""
-        return self.distribution.pdf(X - self.bias, **self.parameters)
+        return self.distribution.pdf(X, **self.parameters)
 
     def cdf(self, X):
         """Compute the member CDF."""
-        return self.distribution.cdf(X - self.bias, **self.parameters)
+        return self.distribution.cdf(X, **self.parameters)

mixture_model/gaussian_mixture.py
@@ -52,19 +52,28 @@ def _count_unique_items(items):
 class GaussianMixtureModel(MixtureModel):
     """Base class for Gaussian Mixtures."""
 
-    def __init__(self, grouping=None):
+    def __init__(self, grouping, member_names=None):
         """Initialize a univariate normal Gaussian Mixture Model.
 
         Parameters
         ----------
         grouping : list of integers specifying to which group a model belongs
         """
-        member_count = _count_unique_items(grouping)
+        member_count = len(grouping)
         super().__init__(member_count, norm)
+        self.grouping = grouping
+        if member_names is None:
+            member_names = self._create_default_member_names()
+        assert len(member_names) == len(grouping)
+
         self._forecast_prepared = False
         self.member_count = member_count
-        self.grouping = grouping
+
         self._optimizer = GaussianEM(grouping)
+
+    def _create_default_member_names(self):
+        # TODO TdR 14-06-2016: Use grouping in names
+        return ['member_' + str(x) for x in range(1, len(self.grouping) + 1)]
 
     def fit(self, training_features, training_observations):
         """Fitting the GMM with the provided data.
@@ -90,7 +99,7 @@ class GaussianMixtureModel(MixtureModel):
         # Update variances
         for (member, model_std) in \
             zip(self._members,
-                self._optimizer.get_member_variances()):
+                np.sqrt(self._optimizer.get_member_variances())):
             member.parameters['scale'] = model_std
 
         # Update weights
@@ -117,15 +126,15 @@ class GaussianMixtureModel(MixtureModel):
     def set_member_variances(self, member_variances):
         """Modify the member variances explicitly."""
         assert len(member_variances) == self.member_count
-        for (variance, member) in zip(member_variances, self._members):
-            member.parameters['scale'] = variance
+        for (std, member) in zip(np.sqrt(member_variances), self._members):
+            member.parameters['scale'] = std
 
     def get_member_variances(self):
         """Return fitted member variances."""
-        return [
+        return np.square([
             member.parameters['scale']
             for member in self._members
-        ]
+        ])
 
     def mean(self):
         """Return the GMM mean forecast."""

tests/test_gaussian_mixture.py
@@ -12,7 +12,8 @@ class TestGaussianMixtureModel:
     def setup_class(self):
         """Mock a simple base class."""
         from mixture_model.gaussian_mixture import GaussianMixtureModel
-        self.model = GaussianMixtureModel(50)
+        mixture_model_grouping = np.arange(0, 50)
+        self.model = GaussianMixtureModel(mixture_model_grouping)
         self.model.set_member_means([0] * 50)
 
     def test_cdf_upper_boundary(self):
@@ -42,6 +43,11 @@ class TestGaussianEM:
         errors = np.arange(-1, 1, 0.01)
         goal_values = norm.logpdf(errors, loc=0, scale=1)
         attempt_values = _log_normal_pdf(errors**2, 1)
+        assert_almost_equal(goal_values, attempt_values)
+
+        # Comparison with a zero-mean wide normal distribution.
+        goal_values = norm.logpdf(errors, loc=0, scale=4)
+        attempt_values = _log_normal_pdf(errors**2, 4**2)
         assert_almost_equal(goal_values, attempt_values)
 
     def test_log_normal_pdf_is_isotropic(self):